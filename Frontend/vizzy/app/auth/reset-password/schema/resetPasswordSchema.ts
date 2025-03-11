83f554369a5bdaf5651f355b2642a5e9eff0604a
zealves99 <a27967@alunos.ipca.pt>
2025-03-11 11:23:06 +0000

    reset-password page
    Front-end of the reset-password page.

---
+++ Frontend/vizzy/app/auth/reset-password/schema/resetPasswordSchema.ts
@@ -0,0 +1,9 @@
+import z from 'zod';
+
+export const resetPasswordSchema = z.object({
+  email: z.string().email({
+    message: 'Please enter a valid email address.',
+  }),
+});
+
+export type FormValues = z.infer<typeof resetPasswordSchema>;
