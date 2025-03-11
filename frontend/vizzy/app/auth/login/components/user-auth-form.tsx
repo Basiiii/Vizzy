f63e8b5a0be8d828fdc57308be16647c2e951b6d
DiogoMachado04 <a26042@alunos.ipca.pt>
2025-03-11 00:30:42 +0000

    Criação da pagina de Login
    Criação da pagina de Login com validação dos valores dos formulários

---
+++ frontend/vizzy/app/auth/login/components/user-auth-form.tsx
@@ -0,0 +1,145 @@
+'use client';
+
+import * as React from 'react';
+import { Eye, EyeOff, Loader2 } from 'lucide-react';
+import { cn } from '@/lib/utils';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+  userAuthFormSchema,
+  FormValues,
+} from '@/app/auth/login/schema/user-auth-form-schema';
+
+type UserAuthFormProps = React.HTMLAttributes<HTMLDivElement>;
+
+export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+  const [showPassword, setShowPassword] = React.useState<boolean>(false);
+  React.useState<boolean>(false);
+
+  /**
+   * Initializes the form using `react-hook-form` with validation powered by `zod`.
+   * The form is tied to the `userAuthFormSchema` validation schema.
+   * The default form values are set for `email` and `password`.
+   *
+   * @type {ReturnType<typeof useForm<FormValues>>} The form hook instance that provides form methods and state.
+   */
+  const form = useForm<FormValues>({
+    resolver: zodResolver(userAuthFormSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  /**
+   * Handles form submission for logging in.
+   * This function is triggered when the form is submitted, sends the form data to the Supabase API,
+   * and handles the response or errors accordingly.
+   *
+   * @param {FormValues} values - The form values containing email and password.
+   * @returns {Promise<void>} - A promise that resolves when the submission is complete.
+   */
+  async function onSubmit(values: FormValues): Promise<void> {
+    // Set loading state to true when the form is being submitted
+    setIsLoading(true);
+
+    console.log(values);
+
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }
+
+  return (
+    <div className={cn('grid gap-6', className)} {...props}>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="sr-only">Email</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Email"
+                    type="email"
+                    autoCapitalize="none"
+                    autoComplete="email"
+                    autoCorrect="off"
+                    disabled={isLoading}
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="sr-only">Password</FormLabel>
+                <div className="relative">
+                  <FormControl>
+                    <Input
+                      placeholder="Password"
+                      type={showPassword ? 'text' : 'password'}
+                      autoCapitalize="none"
+                      autoComplete="new-password"
+                      autoCorrect="off"
+                      disabled={isLoading}
+                      {...field}
+                    />
+                  </FormControl>
+                  <Button
+                    type="button"
+                    variant="ghost"
+                    size="sm"
+                    className="absolute right-0 top-0 h-full px-3 py-2 hover:bg-transparent"
+                    onClick={() => setShowPassword(!showPassword)}
+                    disabled={isLoading}
+                  >
+                    {showPassword ? (
+                      <EyeOff className="h-4 w-4" aria-hidden="true" />
+                    ) : (
+                      <Eye className="h-4 w-4" aria-hidden="true" />
+                    )}
+                    <span className="sr-only">
+                      {showPassword ? 'Hide password' : 'Show password'}
+                    </span>
+                  </Button>
+                </div>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button type="submit" className="w-full" disabled={isLoading}>
+            {isLoading ? (
+              <>
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                Logging in...
+              </>
+            ) : (
+              'Log in'
+            )}
+          </Button>
+        </form>
+      </Form>
+    </div>
+  );
+}
