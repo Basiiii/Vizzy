12135d3dc3ac1b45f4f617454afc667becf23aaf
DiogoMachado04 <a26042@alunos.ipca.pt>
2025-03-17 14:03:10 +0000

    Documentação de função
    Documentação da função logInUser.

--- frontend/vizzy/app/auth/login/utils/login-user.ts
+++ frontend/vizzy/app/auth/login/utils/login-user.ts
@@ -1,3 +1,12 @@
+/**
+ * This function sends a login request to the /api/auth/login endpoint using a POST request.
+ * It includes the user's email and password in the request body. If the response is successful, it returns the login data.
+ * If the login fails, it throws an error with an appropriate message.
+ * @param email - The user's email address
+ * @param password - The user's password
+ * @returns {Promise<void>} - A promise that resolves when the user is successfully logged in.
+ * @throws {Error} - Throws an error if the log in failed.
+ */
 export async function LogInUser(email: string, password: string) {
   const response = await fetch('/api/auth/login', {
     method: 'POST',
