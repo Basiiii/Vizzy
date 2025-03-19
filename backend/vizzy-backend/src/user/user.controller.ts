6354538ca080d15cb3388851fb6abb91d96b14fc
Enrique R. (Basi) <hello.basi@proton.me>
2025-03-19 21:29:32 +0000

    Update user.controller.ts

--- backend/vizzy-backend/src/user/user.controller.ts
+++ backend/vizzy-backend/src/user/user.controller.ts
@@ -1,13 +1,8 @@
-import { Controller, Delete, Get, Param, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './models/user.model';
 import { JwtAuthGuard } from '../auth/guards/jwt.auth.guard';
-import { Request } from 'express';
 import { SupabaseService } from 'src/supabase/supabase.service';
-
-interface CustomRequest extends Request {
-  cookies: Record<string, string>;
-}
 
 @Controller('user')
 export class UserController {
@@ -15,6 +10,7 @@ export class UserController {
     private readonly userService: UserService,
     private readonly supabaseService: SupabaseService,
   ) {}
+
   @Get(':id')
   async getUser(@Param('id') id: string): Promise<User | null> {
     return this.userService.getUserById(id);
@@ -24,16 +20,5 @@ export class UserController {
   @UseGuards(JwtAuthGuard)
   async getMe(@Param('id') id: string): Promise<User | null> {
     return this.userService.getUserById(id);
-  }
-
-  @Delete('delete')
-  deleteUSer(
-    @Req() req: CustomRequest,
-  ) /*: Promise<{ message: string } | { error: string }>*/ {
-    SupabaseService;
-    const jwtToken = req.cookies?.['arroz'];
-
-    //const token = req.headers.values;
-    //return this.userService.deleteUser(token);
   }
 }
