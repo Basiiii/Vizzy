88e27d18f6a959e7f88697d5174abff75536a2c6
DiogoMachado04 <a26042@alunos.ipca.pt>
2025-04-03 16:31:26 +0100

    Alterações no controller

--- backend/vizzy-backend/src/transaction/transaction.controller.ts
+++ backend/vizzy-backend/src/transaction/transaction.controller.ts
@@ -3,11 +3,13 @@ import {
   Get,
   NotFoundException,
   Query,
+  UseGuards,
   Version,
 } from '@nestjs/common';
 import { TransactionService } from './transaction.service';
 import { API_VERSIONS } from '@/constants/api-versions';
 import { Transaction } from '@/dtos/transaction/transaction.dto';
+import { JwtAuthGuard } from '@/auth/guards/jwt.auth.guard';
 
 @Controller('transactions')
 export class TransactionController {
@@ -38,5 +40,19 @@ export class TransactionController {
       throw new NotFoundException('No transactions found for this user');
     }
     return transactions;
+  }
+
+  @Get('value')
+  @Version(API_VERSIONS.V1)
+  @UseGuards(JwtAuthGuard)
+  async getTransactionValue(@Query('userId') userId: string): Promise<number> {
+    if (!userId) {
+      throw new NotFoundException('User ID is required');
+    }
+
+    if (count === 0) {
+      throw new NotFoundException('No transactions found for this user');
+    }
+    return count;
   }
 }
