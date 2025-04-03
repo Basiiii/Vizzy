9687e259b760d671dbbf591fb637eb8a1f124d0d
Diogo Abreu <a27975@alunos.ipca.pt>
2025-04-03 21:31:39 +0100

    Create endpoint

---
+++ backend/vizzy-backend/src/transactions/transactions.controller.ts
@@ -0,0 +1,52 @@
+import {
+  Controller,
+  Get,
+  Query,
+  NotFoundException,
+  Version,
+  Inject,
+} from '@nestjs/common';
+import { TransactionsService } from './transactions.service';
+import { Listing } from '@/dtos/listing/listing.dto';
+import { API_VERSIONS } from '@/constants/api-versions';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import { Logger } from 'winston';
+
+@Controller('Transactions')
+export class TransactionsController {
+  constructor(
+    private readonly transactionsService: TransactionsService,
+    @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
+  ) {}
+
+  @Get()
+  @Version(API_VERSIONS.V1)
+  async getListings(
+    @Query('userid') userId: string,
+    @Query('page') page = '1',
+    @Query('limit') limit = '8',
+  ): Promise<Listing[]> {
+    this.logger.info(`Using controller getTransactions for user ID: ${userId}`);
+    if (!userId) {
+      this.logger.warn('User ID is required but not provided');
+      throw new NotFoundException('User ID is required');
+    }
+
+    const options = {
+      limit: parseInt(limit, 10),
+      offset: (parseInt(page, 10) - 1) * parseInt(limit, 10),
+    };
+
+    const transactions = await this.transactionsService.getUserTransactionById(
+      userId,
+      options,
+    );
+
+    if (!transactions.length) {
+      this.logger.warn(`No transactions found for user ID: ${userId}`);
+      throw new NotFoundException('No transactions found for this user');
+    }
+
+    return transactions;
+  }
+}
