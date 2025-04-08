77c81ec75b6e20feeeae0852d876f731ab40c462
Enrique R. (Basi) <hello.basi@proton.me>
2025-04-08 23:12:21 +0100

    fix: navbar in marketplace page
    Navbar in marketplace was not getting user metadata

--- frontend/vizzy/app/page.tsx
+++ frontend/vizzy/app/page.tsx
@@ -1,11 +1,14 @@
 import { Footer } from '@/components/layout/footer';
 import NavBar from '@/components/layout/nav-bar/nav-bar';
 import Marketplace from '@/components/marketplace/marketplace';
+import { getServerUser } from '@/lib/utils/token/get-server-user';
 
-export default function Home() {
+export default async function Home() {
+  const userMetadata = await getServerUser();
+
   return (
     <main>
-      <NavBar username={''} avatarUrl={''} />
+      <NavBar username={userMetadata?.username || ''} avatarUrl={''} />
       <div className="pt-12">
         <Marketplace />
       </div>
