7fe9cb7cbe4d4f8928dd404b42e7009f816dcd17
Enrique R. (Basi) <hello.basi@proton.me>
2025-04-11 17:24:06 +0100

    fix: production cookies

--- backend/vizzy-backend/src/auth/helpers/cookie.helper.ts
+++ backend/vizzy-backend/src/auth/helpers/cookie.helper.ts
@@ -7,18 +7,21 @@ export class CookieHelper {
     accessToken?: string,
     refreshToken?: string,
   ): void {
-    const isProduction = process.env.NODE_ENV === 'production';
+    // TODO: confirm if we really need this
+    // const isProduction = process.env.NODE_ENV === 'production';
 
     res.cookie(AUTH_COOKIES.ACCESS_TOKEN, accessToken || '', {
-      secure: isProduction,
-      sameSite: 'lax',
+      secure: true,
+      sameSite: 'none',
+      // httpOnly: true,
       maxAge: 60 * 60 * 1000, // 1 hour
       path: '/',
     });
 
     res.cookie(AUTH_COOKIES.REFRESH_TOKEN, refreshToken || '', {
-      secure: isProduction,
-      sameSite: 'lax',
+      secure: true,
+      sameSite: 'none',
+      // httpOnly: true,
       maxAge: 60 * 60 * 24 * 30 * 1000, // 30 days
       path: '/',
     });
