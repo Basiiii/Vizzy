514383d12cddd7d972d4dd624f3d92d3ac1ef55b
DiogoMachado04 <a26042@alunos.ipca.pt>
2025-04-12 14:24:48 +0100

    feat: endpoint call in frontend

--- frontend/vizzy/lib/api/listings/create-listing.ts
+++ frontend/vizzy/lib/api/listings/create-listing.ts
@@ -1,7 +1,9 @@
 import { getClientCookie } from '@/lib/utils/cookies/get-client-cookie';
 import { createAuthHeaders } from '@/lib/api/core/client';
-
-export async function updateProposalStatus(status: string, proposalId: number) {
+import { CreateListingDto } from '@/types/create-listing';
+export async function createListing(
+  createListing: CreateListingDto,
+): Promise<number> {
   const API_URL = process.env.NEXT_PUBLIC_API_URL;
   const API_VERSION = process.env.NEXT_PUBLIC_API_VERSION;
   const token = getClientCookie('auth-token');
@@ -11,17 +13,16 @@ export async function updateProposalStatus(status: string, proposalId: number) {
 
   const headers = createAuthHeaders(token);
 
-  const response = await fetch(
-    `${API_URL}/${API_VERSION}/proposals/update-status`,
-    {
-      method: 'PUT',
-      headers: headers,
-      credentials: 'include',
-      body: JSON.stringify({ status, proposalId }),
-    },
-  );
+  const response = await fetch(`${API_URL}/${API_VERSION}/listings`, {
+    method: 'POST',
+    headers: headers,
+    credentials: 'include',
+    body: JSON.stringify(createListing),
+  });
 
   if (!response.ok) {
-    throw new Error('Update proposal status failed');
+    throw new Error('Failed to create listing');
   }
+
+  return await response.json();
 }
