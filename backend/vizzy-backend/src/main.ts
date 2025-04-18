a7bd11162629e77f51b1c584a8062f77ba3f7aa1
Enrique R. (Basi) <hello.basi@proton.me>
2025-04-18 21:10:50 +0100

    refactor: allow all origins to api

--- backend/vizzy-backend/src/main.ts
+++ backend/vizzy-backend/src/main.ts
@@ -48,17 +48,18 @@ async function bootstrap() {
   }
 
   app.enableCors({
-    origin: (
-      origin: string,
-      callback: (arg0: Error, arg1: boolean) => unknown,
-    ) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) > -1) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'), false);
-      }
-    },
+    // origin: (
+    //   origin: string,
+    //   callback: (arg0: Error, arg1: boolean) => unknown,
+    // ) => {
+    //   if (!origin) return callback(null, true);
+    //   if (allowedOrigins.indexOf(origin) > -1) {
+    //     callback(null, true);
+    //   } else {
+    //     callback(new Error('Not allowed by CORS'), false);
+    //   }
+    // },
+    origin: true,
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
